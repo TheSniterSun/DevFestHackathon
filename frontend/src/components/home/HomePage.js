fdc4cf96928bccf92e520a847e1b9ec84b07ac09
paeb37 <btp2109@columbia.edu>
2024-02-03 12:47:17 -0500

    updated files

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -15,7 +15,7 @@ import { Link } from 'react-router-dom';
 const baseURL = "http://localhost:8000"; // for local dev this is the backend server
 
 function HomePage(props) {
-    const logoPath = '/images/secra_logo.png'; // in public folder
+    const logoPath = '/images/wewater.png'; // in public folder
     const token = props.token;
     const isLoggedIn = user_authorized(token);
 
@@ -24,23 +24,22 @@ function HomePage(props) {
         <br />
         <br />
 
-        <h1 className={styles.textCenter}>Welcome to Secra!</h1>
+        <h1 className={styles.textCenter}>Welcome to WeWater!</h1>
 
         <div className={styles.imageCenter}>
             <img src={process.env.PUBLIC_URL + logoPath} className={styles.center} alt="logo" />
         </div>
 
         <div className={styles.textCenter}>
-            <h2>Secra is an AI scheduling assistant <span className={styles.homeLightBlueText}>for students</span>. 
-            It has 3 main features: </h2>
+            <h2>WeWater is a place to report water quality concerns (bacteria like e.coli or reduced quality) so other people can see if it's just the neighborhood or if it's a city/community wide event </h2>
 
             <br />
 
             <h2 className={styles.centerContainer}>
                 <ul className={styles.noBullets}>
-                    <li> <span className={`${styles.homeDarkBlueText} ${styles.boldText}`}>Chat:</span> Schedule events and meetings</li>
-                    <li> <span className={`${styles.homeDarkBlueText} ${styles.boldText}`}>Canvas:</span> Plan times to work on assignments</li>
-                    <li> <span className={`${styles.homeDarkBlueText} ${styles.boldText}`}>Connect:</span> Talk with other student assistants</li>
+                    <li> <span className={`${styles.homeDarkBlueText} ${styles.boldText}`}>Step 1:</span> Report water concerns </li>
+                    <li> <span className={`${styles.homeDarkBlueText} ${styles.boldText}`}>Step 2:</span> Recieve warnings </li>
+                    <li> <span className={`${styles.homeDarkBlueText} ${styles.boldText}`}>Step 3:</span> Contact officials </li>
                 </ul>
             </h2>
 
--- notes.md
+++ notes.md
@@ -1,49 +1,12 @@
-Virtual Env:
+To run:
 
-OLD (ONLY TO CREATE A NEW ENV)
-1. python3 -m venv .venv
+Backend:
+
+1. Go to "backend" folder
 2. . .venv/bin/activate
-3. pip install Flask
+3. python3 run.py
 
-NEW
-1. . .venv/bin/activate
+Frontend:
 
-Conda (DO NOT USE)
-
-1. conda create --name secra --clone base
-2. conda activate secra
-3. conda deactivate
-
-Flask:
-
-https://flask.palletsprojects.com/en/3.0.x/installation/
-
-pip install Flask
-
-https://flask.palletsprojects.com/en/3.0.x/quickstart/
-
-React:
-
-npx create-react-app frontend
-
-Testing:
-
-*Backend:*
-
-python3 run.py
-
-*Frontend:*
-
-npm start
-
-SSH
-
-1. Move "mysql.pem" file into your local .ssh folder
-2. 
-
-*INSTALLATIONS*
-1. 
-
-*Queries:*
-
-Schedule a 15 minute study session for Thursday, repeating every week. The location is Uris Hall
+1. Go to "frontend" folder
+2. npm start
